455dc95ea70a451d308a0b26de90fdee27915007
colorplease <74271202+colorplease@users.noreply.github.com>
2022-06-23 21:12:52 -0700

    initialize

---
+++ belly up/Assets/Scripts/aim.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aim : MonoBehaviour
+{
+    Vector2 mousePos;
+    public Rigidbody2D rb;
+    public float difference;
+
+    // Update is called once per frame
+    void Update()
+    {
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector2 lookDir = mousePos - rb.position;
+            float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg  - difference;
+            rb.rotation = angle;
+    }
+}
