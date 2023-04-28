e4aebab917beeeb64880d43aca08f3ea920690db
colorplease <74271202+colorplease@users.noreply.github.com>
2023-04-27 22:25:10 -0700

    fish guidance

--- belly up/Assets/Scripts/ohthefishguide.cs
+++ belly up/Assets/Scripts/ohthefishguide.cs
@@ -1,18 +1,95 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class ohthefishguide : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]TextMeshProUGUI title;
+    [SerializeField]TextMeshProUGUI type;
+    [SerializeField]TextMeshProUGUI occupation;
+    [SerializeField]TextMeshProUGUI education;
+    [SerializeField]TextMeshProUGUI hobbies;
+    [SerializeField]TextMeshProUGUI tagline;
+    [SerializeField]GameObject[] data;
+    [SerializeField]GameObject[] fish3D;
+    public void cod()
     {
-        
+        title.SetText("Cod");
+        type.SetText("Type: fish");
+        occupation.SetText("Occupation: fish");
+        education.SetText("Education: Business Major at Standford");
+        hobbies.SetText("Hobbies: fish");
+        tagline.SetText("i have no adversaries");
+        for(int i = 0; i < data.Length; i++)
+        {
+            data[i].SetActive(false);
+        }
+        data[0].SetActive(true);
+        for(int i = 0; i < fish3D.Length; i++)
+        {
+            fish3D[i].SetActive(false);
+        }
+        fish3D[0].SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void sword()
     {
-        
+        title.SetText("Sword Fish");
+        type.SetText("Type: fish");
+        occupation.SetText("Occupation: Data Science Analyst");
+        education.SetText("Education: fish");
+        hobbies.SetText("Hobbies: fish");
+        tagline.SetText("i want a girlfriend");
+        for(int i = 0; i < data.Length; i++)
+        {
+            data[i].SetActive(false);
+        }
+        data[1].SetActive(true);
+        for(int i = 0; i < fish3D.Length; i++)
+        {
+            fish3D[i].SetActive(false);
+        }
+        fish3D[1].SetActive(true);
+    }
+
+    public void angler()
+    {
+        title.SetText("Angler Fish");
+        type.SetText("Type: fish");
+        occupation.SetText("Occupation: fish");
+        education.SetText("Education: fish");
+        hobbies.SetText("Hobbies: reading and dabbles in writing");
+        tagline.SetText("perpendicularity of the bisector");
+        for(int i = 0; i < data.Length; i++)
+        {
+            data[i].SetActive(false);
+        }
+        data[2].SetActive(true);
+        for(int i = 0; i < fish3D.Length; i++)
+        {
+            fish3D[i].SetActive(false);
+        }
+        fish3D[2].SetActive(true);
+    }
+
+    public void blobfish()
+    {
+        title.SetText("Blobfish");
+        type.SetText("Type: fish");
+        occupation.SetText("Occupation: Business and Finance Consultant");
+        education.SetText("Education: fish");
+        hobbies.SetText("Hobbies: fish");
+        tagline.SetText("hello francis");
+        for(int i = 0; i < data.Length; i++)
+        {
+            data[i].SetActive(false);
+        }
+        data[3].SetActive(true);
+        for(int i = 0; i < fish3D.Length; i++)
+        {
+            fish3D[i].SetActive(false);
+        }
+        fish3D[3].SetActive(true);
     }
 }
