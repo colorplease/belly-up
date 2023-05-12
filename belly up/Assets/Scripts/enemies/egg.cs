76b41e6607ec9c4805e843c4175a6c2237608c57
colorplease <74271202+colorplease@users.noreply.github.com>
2023-05-12 11:08:39 -0500

    v0.3.1.1T

--- belly up/Assets/Scripts/enemies/egg.cs
+++ belly up/Assets/Scripts/enemies/egg.cs
@@ -10,7 +10,7 @@ public class egg : MonoBehaviour
     public void Spawn(int limit)
     {
         mommy = GameObject.FindWithTag("mommy").GetComponent<Transform>();
-        GameObject fish = Instantiate(enemies[Random.Range(0, limit - 1)], transform.position, Quaternion.identity);
+        GameObject fish = Instantiate(enemies[Random.Range(0, limit)], transform.position, Quaternion.identity);
         fish.transform.parent = mommy;
         Destroy(gameObject);
     }
@@ -18,7 +18,7 @@ public class egg : MonoBehaviour
     {
         mommy = GameObject.FindWithTag("mommy").GetComponent<Transform>();
         //[0,1,3,4];
-        GameObject fish = Instantiate(enemies[validChoices[Random.Range(0, limit - 1)]], transform.position, Quaternion.identity);
+        GameObject fish = Instantiate(enemies[validChoices[Random.Range(0, limit)]], transform.position, Quaternion.identity);
         fish.transform.parent = mommy;
         Destroy(gameObject);
     }
